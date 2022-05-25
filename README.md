4182bee6d9c86139bf29ca5d423ab4b929c958c6
Isidore Mohr <henrymohr7@gmail.com>
2022-05-25 17:16:25 -0400

    Game of Life implementation

---
+++ README.md
@@ -0,0 +1,35 @@
+# Introduction
+
+This is a simple Python implementation of [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life).
+The game itself is very simple. 
+It requires no players, instead evolving according to specified rules from a starting configuration.
+The board is divided into cells which can be "alive" or "dead".
+At each step, each cell evolves based on its state and its neighbors' states.
+
+# Game Rules:
+
+1. An alive cell with either two or three neighbors stays alive.
+2. A dead cell with exactly three live neighbors becomes alive.
+3. All other cells die (if alive) or stay dead.
+
+# How to Interact
+
+On starting the game, it will generate a 20x20 empty board.
+The state of the board can be modified with commands and then run or stepped through.
+Commands can be chained together with semicolons.
+The state of the board will be displayed after each command.
+
+You can record sequences of commands as macros. Macros are saved in macros.json.
+The starting macros are "glider" (creates a glider in the top left, assuming it is empty)
+and "genesis" (fills the board with random soup then runs for 100 turns with 0.15 delay).
+
+The commands are:
+stop - exit the program
+step [<n>] - take <n> game steps without displaying intermediate results; if n is omitted it will be 1
+run <n> [<d>] - run and display n game steps with a delay of d; if d is omitted it will be 0.3
+flip <x> <y> - flip the state at cell (x, y)
+board <n> - set the board to be an empty n by n square
+soup [<n>] - set the board to be an n by n square with random contents. If n is omitted it will be the current board size.
+record - begin recording a macro
+end <name> - saves the current macro as <name>. It can then be invoked as a command.
+help - prints a help message. This is mostly redundant with this document but will contain a list of extant macros.
---
+++ conway.py
@@ -0,0 +1,164 @@
+from ast import arg
+import numpy as np
+import re, random, json, time
+# from graphics import *
+
+# fuck it we'll do this as a list of lists
+
+def step(array):
+    new_array = [0] * len(array)
+    for i, row in enumerate(new_array):
+        new_array[i] = [False] * len(array[0])  # assumes the array is at least 1x1
+    for i, row in enumerate(array):
+        for j, cell in enumerate(row):
+            neighbors = {True: 0, False: 0}
+            if i > 0:
+                neighbors[array[i-1][j]] += 1
+                if j > 0:
+                    neighbors[array[i-1][j-1]] += 1
+                if j < len(array[0]) - 1:
+                    neighbors[array[i-1][j+1]] += 1
+            if j > 0:
+                neighbors[array[i][j-1]] += 1
+                if i < len(array) - 1:
+                    neighbors[array[i+1][j-1]] += 1
+            if i < len(array) - 1:
+                neighbors[array[i+1][j]] += 1
+                if j < len(array[0]) - 1:
+                    neighbors[array[i+1][j+1]] += 1
+            if j < len(array[0]) - 1:
+                neighbors[array[i][j+1]] += 1
+            if cell:
+                if neighbors[True] < 2:
+                    new_array[i][j] = False
+                elif neighbors[True] > 3:
+                    new_array[i][j] = False
+                else:
+                    new_array[i][j] = True
+            else:
+                if neighbors[True] == 3:
+                    new_array[i][j] = True
+                else:
+                    new_array[i][j] = False
+    return new_array
+
+def repl(board):
+    commands = []
+    recording = False
+    with open('macros.json', 'r') as macros_file:
+        macros = json.load(macros_file)
+    current_macro = []
+    while True:
+        print_board(board)
+        if not commands:
+            control = input('Enter command or "help": ')
+            commands = [comm.strip() for comm in control.split(';')]
+        else:
+            print(commands[0])
+        if recording:
+            current_macro.append(commands[0])
+        argc = commands[0].split(' ')
+        if argc[0] == "help" or argc[0] == '"help"':
+            print("For more information, see README.md")
+            print("Commands can be chained together with ;")
+            print("COMMAND LIST:")
+            print("stop - exit the program")
+            print("step [<n>] - take <n> game steps; if n is omitted it will be 1")
+            print("run <n> [<d>] - display n game steps with a delay of d; if d is omitted it will be 1")
+            print("flip <x> <y> - flip the state at cell (x, y)")
+            print("board <n> - set the board to be an empty n by n square")
+            print("soup <n> - set the board to be an n by n square with random contents")
+            print("record - begin recording a macro")
+            print("end <name> - saves the current macro as <name>. It can then be invoked as a command.")
+            print(f"Current macros: {', '.join(m for m in macros)}")
+            print("help - print this message")
+        elif argc[0] == 'step':
+            if len(argc) > 2:
+                print('Usage: step [<n>]')
+            elif len(argc) == 2:
+                n = int(argc[1])
+                for i in range(n):
+                    board = step(board)
+            else:
+                board = step(board)
+        elif argc[0] == 'run':
+            if len(argc) > 3 or len(argc) < 2:
+                print("Usage: run <n> [<d>]")
+            else:
+                n = int(argc[1])
+                if len(argc) == 3:
+                    d = float(argc[2])
+                else:
+                    d = 0.3
+                for i in range(n):
+                    print(i + 1)
+                    board = step(board)
+                    if i < n - 1:
+                        print_board(board)
+                        time.sleep(d)
+        elif argc[0] == 'flip':
+            try:
+                x = int(argc[1])
+                y = int(argc[2])
+                board[x][y] = not board[x][y]
+            except ValueError:
+                print(f'Could not parse {argc[1]} and {argc[2]} as integers')
+            except IndexError:
+                print('Not a point on the board!')
+        elif argc[0] == 'board':
+            board = make_board(int(argc[1]))
+        elif argc[0] == 'soup':
+            if len(argc) > 2:
+                print('Usage: soup [<n>]')
+            else:
+                if len(argc) == 2:
+                    size = argc[1]
+                else:
+                    size = len(board)
+                board = make_board(int(size), soup=True)
+        elif argc[0] == 'record':
+            recording = True
+        elif argc[0] == 'end':
+            if recording:
+                recording = False
+                macros[argc[1]] = ';'.join(current_macro[:-1])  # [:-1] drops the end macro command
+                with open('macros.json', 'w') as macros_file:
+                    json.dump(macros, macros_file)
+                current_macro = ''
+            else:
+                print("Not currently recording")
+        elif argc[0] == "stop":
+            break
+        elif argc[0] in macros:
+            commands = [commands[0]] + [c.strip() for c in macros[argc[0]].split(';')] + commands[1:]
+        else:
+            print("Not a valid command")
+        commands = commands[1:]
+
+def print_board(board):
+    board_str = '  ' + ' '.join([str(i % 10) for i in range(len(board))]) + '\n'
+    for i, row in enumerate(board):
+        board_str += f'{i % 10} ' + ' '.join(['▮' if cell else '.' for cell in row]) + '\n'
+    print(board_str, end=None)
+
+# def print_board(board):
+#     win = GraphWin('Game of Life', len(board) * 10, len(board[0]) * 10)
+#     win.setCoords(0, 0, len(board), len(board[0]))
+#     for i in range(len(board)):
+#         for j in range(len(board[0])):
+#             cell = Rectangle(Point(i, i+1), Point(j, j+1))
+#             cell.draw(win)
+#     win.getMouse()
+
+def make_board(n, soup=False):
+    board = [False] * n
+    for i, row in enumerate(board):
+        if soup:
+            board[i] = [random.randint(0, 1) == 0 for _ in range(n)]
+        else:
+            board[i] = [False] * n
+    return board
+ 
+
+if __name__ == '__main__':
+    repl(make_board(20))
